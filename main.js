de4a8fb69b4d2371a786eebe8f278381d45e15a9
obiot <olivier.biot@gmx.com>
2011-12-18 00:51:18 +0800

    Added the TCB Loader (Issue #8)

--- main.js
+++ main.js
@@ -18,27 +18,49 @@ var jsApp	=
 	onload: function()
 	{
 		
-      // init the video
+		// init the video
 		if (!me.video.init('jsapp', 640, 400, false, 1.0))
 		{
 			alert("Sorry but your browser does not support html 5 canvas.");
-         return;
+			return;
 		}
 				
 		// initialize the "audio"
 		//me.audio.init("mp3,ogg");
-		var player = new music("YM");
-		player.LoadAndRun('data/music/Cuddly - main menu.ym');
-
+		this.player = new music("YM");
 		
-		// set all resources to be loaded
-		me.loader.onload = this.loaded.bind(this);
-		
-		// set all resources to be loaded
-		me.loader.preload(g_resources);
+		// get a ref to the canvas
+		var ctx = me.video.getScreenFrameBuffer();
+		// clear surface
+		me.video.clearSurface(ctx, "black");
+		// display a centered "please wait"
+		var font = new me.Font('courier', 11, 'white');
+		var dim  = font.measureText(ctx, 'PLEASE WAIT');
+		font.draw(ctx, 'PLEASE WAIT', ((ctx.canvas.width - dim.width) / 2),  (ctx.canvas.height) / 2);
+						
+		// manually load the background, since we need it to be loaded for the loader...
+		me.loader.load({name: "loader", type:"image", src: "data/background/loader.png"}, function() 
+		{
+			// same for the brown font
+			me.loader.load({name: "loader_font_brown", type:"image", src: "data/fonts/fonts15x16_brown.png"}, function() 
+			{
+				// and for the white font
+				me.loader.load({name: "loader_font_white", type:"image", src: "data/fonts/fonts15x16.png"}, function() 
+				{
+					// set all resources to be loaded
+					me.loader.onload = jsApp.loaded.bind(jsApp);
+				
+					// set all resources to be loaded
+					me.loader.preload(g_resources);
 
-		// load everything & display a loading screen
-		me.state.change(me.state.LOADING);
+					// set our custom loader
+					me.state.set(me.state.LOADING, new TCBLoader());
+				
+					// load everything & display the loading screen
+					me.state.change(me.state.LOADING);
+				}, null);
+			}, null);
+		}, null);
 	},
 	
 	
@@ -49,6 +71,9 @@ var jsApp	=
 		---										*/
 	loaded: function ()
 	{
+		// start the main menu music
+		this.player.LoadAndRun('data/music/Cuddly - main menu.ym');
+		
 		// set the "Play/Ingame" Screen Object
 		me.state.set(me.state.PLAY, new PlayScreen());
       
