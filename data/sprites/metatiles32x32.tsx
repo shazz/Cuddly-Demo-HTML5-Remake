ec67f744b4b04c71f8b5fbb0f1d47daa8b6bebfa
Shazz <shazz.ng@gmail.com>
2011-12-17 17:09:41 +0100

    added level 2 : TCB mindbomb megademo !

---
+++ data/sprites/metatiles32x32.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="metatiles32x32" tilewidth="32" tileheight="32">
+ <image source="metatiles32x32.png" width="192" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="type" value="solid"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" value="platform"/>
+  </properties>
+ </tile>
+</tileset>
