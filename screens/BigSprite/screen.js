397d28ac91938b4eff872b56e691e6a6f9acbf23
Shazz <shazz.ng@gmail.com>
2011-12-30 13:29:32 +0100

    fix logo rotation, scrolltext.

--- screens/BigSprite/screen.js
+++ screens/BigSprite/screen.js
@@ -18,21 +18,19 @@ var BigSpriteScreen = me.ScreenObject.extend({
 	
 		this.text = "   HI, YOU'RE LOOKING AT THE -BIG SPRITE DEMO-.  I WAS CODED BY THE CAREBEARS.  THE MUZEXX WAS DONE BY -MAD MAX- OF THE EXCEPTIONS.  THE \"WHERE-HAVE-I-SEEN-THAT-BEDORE?\"-LOGO WAS DESIGNED BY -ES- OF TEX.   THAT WAS THE CREDITS, NOW WHAT ARE WE GOING TO WRITE ABOUT?   WELL, I CANT HELP IT, I JUST HAVE TO SAY A FEW WORDS ABOUT THE DEFINITIVE DEMO, BY THE LOST BOYS (WE WERE QUITE UPSET WHEN WE GOT IT, AND WE SILL ARE).  IT IS NOT AS BIG AS THIS DEMO, NEITHER IS IT AS GOOD, BUT THEY MANAGED TO RELEASE A FEW FUNNY EFFECTS, THAT HAS NEVER BEEN SEEN ON ON ST BEFORE.  THE MOST IRRITATING ONE IS THEIR TWISTSCROLLER, WHICH IS LOOKS VERY MUSCH LIKE OUR DNA-SCROLLER.   THE PROBLEM WITH CODING DEMOS OF THIS SIZE IS THERE'S A VERY LONG TIME BETWEEN WHEN YOU CODE A SCREEN AND WHEN YOU FINALLY RELEASE IT.  SOME OF THE SCREENS IN THE CUDDLY DEMOS ARE MORE THAN A YEAR OLD.  IF WE WOULD HAVE RELEASED WHEN WE CODED THEM, SAY AS IN INTRO, THEY WOULD HAVE BEEN NEW AND REVOLUTIONARY.  ON THE NO-CREW-COPY-PARTY (WHERE WE CODED THE WHATTAHECK DEMO)  ALL SWEDISH CREWS OF ANY IMPORTANCE SAW ALMOST ALL OUR SCREENS,  SO IF YOU THINK THAT WE'RE STOLEN IDEAS WHEN YOU SEE SIMILARITIES BETWEEN OTHER SWEDISH DEMOS AND THIS DEMO, I'D RATHER SAY IT'S THE OTHER WAY AROUND.  THEY HAVE STOLEN IDEAS FROM OUR DEMO.     NOW, LET'S TALK ABOUT SOMETHING ELSE.  TODAY WE FINALLY GOT MUZEXX FROM MAD MAX OF TEX.  PEOPLE (ESPECIALLY OMEGA) ARE SAYING THAT THERE IS NO UNION.  WE AGREE TO SOME EXTENT - THERE4S NO INTENSIVE COOPERATION BETWEEN THE MEMBERS OF THE UNION. WE DON4T EVEN KNOW THE DELTA FORCE, TNT CREW OR LEVEL 16, EVEN THOUGH WE'D LIKE TO. NEITHER I HAVE SEEN CRACKS BY ANY MEMBER EXCEPT FOR THE REPLICANTS LATELY.  ON THE OTHER HAND, TEX ARE ONE OF THE BEST CODERS, THE REPLICANTS ARE THE BEST CRACKERS AND WE ARE THE ONE OF THEZ BEST (NO, LET'S NOT BE MODEST) THE BEST DEMO-CODERS.  WE GET MUZEXX FROM TEX, AND THEY GET TO KNOW OUR NEW SCROLL-TECHNIQUES.    A FEW MOMENTS AGO, TFE OF OMEGA CALLED AND THAT HE HAD \"BULLSHITTED\" US IN SOME OF THEUR SCROLLTEXTS, BECAUSE WE HAVEN'T SEND OUR LOADER YET (WE TRADED IT FOR THEIR PACKER).  FIRST OF ALL, YOU MUST NOT BELEIVE ANY OF THE BULLSHIT-TCB-TEXTS IN THE OMEGA DEMO.       ";
 		// init canvas and offscreen canvas
-		this.maincanvas=new canvas(me.video.getScreenCanvas()); // reuse melonJS main canvas			
+		this.maincanvas=new canvas(me.video.getScreenCanvas()); // reuse melonJS main canvas	
+		
 		this.scrollcanvas = new canvas(572,52);
-		this.logocanvas1 = new canvas(416,194);
-		this.logocanvas2 = new canvas(416,194);
-		this.logocanvas3 = new canvas(416,194);
 		
 		this.logoUnion = new image(me.loader.getImage('bs_logoUnion'));
-		//this.logoUnion.setmidhandle();
+		this.logoUnion.setmidhandle();
 		this.logoUnion2 = new image(me.loader.getImage('bs_logoUnion2'));
-		//this.logoUnion2.setmidhandle();
+		this.logoUnion2.setmidhandle();
+		
 		this.fontOut = new image(me.loader.getImage('bs_fontOut'));
 		this.fontIn = new image(me.loader.getImage('bs_fontIn'));
 		this.rasters = new image(me.loader.getImage('bs_rasters'));
 
-		
 		this.fontIn.initTile(66,52,32);
 		this.fontOut.initTile(66,52,32);
 		
@@ -54,25 +52,17 @@ var BigSpriteScreen = me.ScreenObject.extend({
 
 		this.scrolltextOut = new scrolltext_horizontal();
 		this.scrolltextOut.scrtxt=this.text;
-		this.scrolltextOut.init(this.scrollcanvas,this.fontOut,8);
+		this.scrolltextOut.init(this.scrollcanvas,this.fontOut,8, undefined, undefined, 32);
 		
 		this.scrolltextIn = new scrolltext_horizontal();
 		this.scrolltextIn.scrtxt=this.text;
-		this.scrolltextIn .init(this.scrollcanvas,this.fontIn,8);	
+		this.scrolltextIn .init(this.scrollcanvas,this.fontIn,8, undefined, undefined, 32);	
 		
 		this.logoAngle = 0;
 		this.rastPos = 0;
 		
-		this.logofxparam1=[
-				{value: Math.PI/2, amp: 1, inc:0.000000, offset: 0.09},
-		];
-		this.logofxparam2=[
-				{value: -Math.PI, amp: -1, inc:0.000000, offset: 0.09},
-		];		
-		this.logofx1 = new FX(this.logocanvas1,this.logocanvas3,this.logofxparam1);
-		this.logoUnion.draw(this.logocanvas1,0,0);
-		this.logofx2 = new FX(this.logocanvas2,this.logocanvas3,this.logofxparam2);
-		this.logoUnion2.draw(this.logocanvas2,0,0);	
+		this.logoZoom = 1.0;
+		this.logoZoomIncr = -0.05;
 		
 		this.showTop = true;
 		this.framecounter = 0;
@@ -80,10 +70,9 @@ var BigSpriteScreen = me.ScreenObject.extend({
 	
 	setRotSpeed : function(val)
 	{
-		if(this.logofxparam1[0].value >= Math.PI/2)
+		if(this.logoZoom >= 1.0)
 		{
-			this.logofxparam1[0].offset = val;
-			this.logofxparam2[0].offset = val;
+			this.logoZoomIncr = -val;
 		}
 	},	
 	
@@ -106,6 +95,20 @@ var BigSpriteScreen = me.ScreenObject.extend({
 		// update rasters
 		this.rastPos += 1.5;
 		if(this.rastPos >= 119) this.rastPos = 0; 
+		
+		this.logoZoom += this.logoZoomIncr;
+		if(this.logoZoom <= -1.0) this.logoZoomIncr = -this.logoZoomIncr;
+		if(this.logoZoom >= 1.0) this.logoZoomIncr = -this.logoZoomIncr;
+
+		
+		if(this.logoZoom <= 0.0)
+		{
+			this.showTop = false;
+		}
+		else
+		{
+			this.showTop = true;
+		}
 		
 		if(this.framecounter >= 72 && this.framecounter < 600)
 		{	
@@ -147,45 +150,19 @@ var BigSpriteScreen = me.ScreenObject.extend({
 		draw function
 	  ---*/
 
-	draw : function(context) {
-		
+	draw : function(context) 
+	{
+		me.video.getScreenCanvas().height = 400+52;
 		this.maincanvas.fill('#000000');
 		this.scrollcanvas.clear();
-		this.logocanvas3.clear();
 		
 		// draw 3D dot starfield
 		this.starfield.draw();		
 		
-		// draw the logo in the offscreen canvas
-		this.logocanvas1.setmidhandle();
-		this.logocanvas2.setmidhandle();
-		this.logocanvas3.setmidhandle();
-
-		if(this.logofxparam1[0].value >= Math.PI)
-		{
-			this.logofxparam1[0].value = 0;
-			this.showTop = false;
-		}
-		if(this.logofxparam2[0].value >= 0)
-		{
-			this.logofxparam2[0].value = -Math.PI;
-			this.showTop = true;
-		}
-		
-		// bad performance fix as zoomy suxx :)
-		if(this.logofxparam1[0].offset != 0.0)
-		{
-			if(this.showTop)
-				this.logofx1.zoomy(0,194/2,1.0);
-			else
-				this.logofx2.zoomy(0,194/2,1.0);
-		}
+		if(this.showTop)
+			this.logoUnion.draw(this.maincanvas, 320 + 120*Math.cos(this.logoAngle), 200 + 80*Math.sin(this.logoAngle*2), 1.0, 0, 1.0, this.logoZoom);
 		else
-		{
-			this.logoUnion.draw(this.logocanvas3,0,0);
-		}
-		
-		this.logocanvas3.draw(this.maincanvas, 320 + 120*Math.cos(this.logoAngle), 200 + 80*Math.sin(this.logoAngle*2));
+			this.logoUnion2.draw(this.maincanvas, 320 + 120*Math.cos(this.logoAngle), 200 + 80*Math.sin(this.logoAngle*2), 1.0, 0, 1.0, this.logoZoom);
 		
 		// draw sprites
 		for(var i=0; i<11; i++)
